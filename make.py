66c45bb1be17bd16dae3d148e1e37d9f3e80ed2a
forbiddenlake <2653277+chennin@users.noreply.github.com>
2023-11-23 08:18:34 -0500

    3.10

--- make.py
+++ make.py
@@ -30,7 +30,7 @@ cwp.vanilla_path = os.path.expanduser( os.path.expandvars( "~/stellaris-game" ) 
 
 MOD_NAME = "Combined Repeatable Techs"
 VERSION = "1"
-SUPPORTED_VERSION = "3.9.*"
+SUPPORTED_VERSION = "3.10.*"
 # 3 = unlisted, 2 = hidden, 1 = friends, 0 = public
 VISIBILITY = 0
 
